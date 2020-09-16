45db01341000431e254dcf0b0f8166d98ed880f0
GAJULA Hema <hema.gajula@hexagon.com>
2020-09-16 14:50:25 +0530

    OverviewUXChanges

--- client/src/features/overview/components/overview-background/OverviewBackground.tsx
+++ client/src/features/overview/components/overview-background/OverviewBackground.tsx
@@ -3,6 +3,7 @@ import { NavButton } from '../../../../shared/components/nav-button/NavButton';
 import { PHDOneLogo } from '../../../../shared/components/phd-one-logo/PHDOneLogo';
 import { PHDTwoLogo } from '../../../../shared/components/phd-two-logo/PHDTwoLogo';
 import reactlogo from '../../../../static/images/hexagon.png';
+import hexdesklogo from '../../../../static/images/hexdesk.png';
 import './overview-background.css';
 import { IOverviewBackgroundProps } from './types/IOverviewBackgroundProps';
 
@@ -31,16 +32,19 @@ export const OverviewBackGround: React.FunctionComponent<IOverviewBackgroundProp
 
     return (
         <div className="overview-background">
+            <div  className="main-head1">
+                <img className="react-image" src={hexdesklogo} />
+                <p className="para_text">Support About Us</p>
+            </div>
             <div className="main-head">
-                <img className="react-image" src={reactlogo} />
-                Welcome to Desk Booking Software
+                Welcome to Work Space Management
                 </div>
             <div className="sub-head"> Helping millions of employees back to the office today. </div>
             <div className="container">
                 <div className="container__right">
-                    <h2>Covid-19 has transformed the way we work forever. Our workspace management software can help get your 
+                    <h4>Covid-19 has transformed the way we work forever. Our workspace management software can help get your 
                         employees back to the office safely. Providing an easy-to-use solution that allows employees to schedule 
-                        desks, find colleagues and notified of changes to their working environment.</h2>
+                        desks, find colleagues and notified of changes to their working environment.</h4>
                     <NavButton buttonText={getButtonText('1')} onClick={phdOneClick} className={"phd-button phd-one"} />
                     <NavButton buttonText={getButtonText('2')} onClick={phdTwoClick} className={"phd-button phd-two"} />
                 </div>
